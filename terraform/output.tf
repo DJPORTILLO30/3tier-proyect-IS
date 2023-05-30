c2e6596e25aed5c9af6ceeb2c5fd97d0c4d539f6
Danilo Portillo <38937343+DJPORTILLO30@users.noreply.github.com>
2023-05-30 14:38:51 -0600

    add public and private subnets

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,3 +5,34 @@ output "vpc_id" {
 output "s3_id" {
     value = aws_s3_bucket.this.id
 }
+
+output "sg-public" {
+  value = aws_security_group.public.id
+}
+
+output "sg-private" {
+  value = aws_security_group.private.id
+}
+
+output "sg-alb-public" {
+  value = aws_security_group.alb-public-sg.id
+}
+
+output "sg-alb-private" {
+  value = aws_security_group.alb-private-sg.id
+}
+
+output "public-subnet-1" {
+  value = aws_subnet.public-subnet-1.id
+}
+
+output "public-subnet-2" {
+  value = aws_subnet.public-subnet-2.id
+}
+
+output "public-private-1" {
+  value = aws_subnet.private-subnet-1.id
+}
+output "public-private-2" {
+  value = aws_subnet.private-subnet-2.id
+}
---
+++ terraform/subnet-private.tf
@@ -0,0 +1,28 @@
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id = "${aws_vpc.this.id}"
+  cidr_block = local.vpc.cidr_subnet_private_1
+  map_private_ip_on_launch = true
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name        = "${local.prefix}-private-subnet-1"
+    Environment = local.env
+    Path        = "${basename(abspath(path.module))}/subnet-private.tf"
+  }
+  depends_on = [ aws_vpc.this ]
+}
+
+
+resource "aws_subnet" "private-subnet-2" {
+  vpc_id = "${aws_vpc.this.id}"
+  cidr_block = local.vpc.cidr_subnet_private_2
+  map_private_ip_on_launch = true
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name        = "${local.prefix}-private-subnet-2"
+    Environment = local.env
+    Path        = "${basename(abspath(path.module))}/subnet-private.tf"
+  }
+  depends_on = [ aws_vpc.this ]
+}
